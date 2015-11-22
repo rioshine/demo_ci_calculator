aaa158c341602fb25744833e6daab9b8126c91f3
RioShine <tnnghia1994@gmail.com>
2015-11-22 20:23:47 +0700

    change main

--- src/main/HelloWorld.java
+++ src/main/HelloWorld.java
@@ -6,7 +6,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Add a = new Add();
-		int result = a.Add(20, 15);
+		int result = a.Add(20, 30);
 		System.out.println(result);
 	}
 	
